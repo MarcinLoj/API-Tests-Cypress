16e3adcd9cd4b24584ac2c1fc9cb065ada97d4ff
MarcinLoj <m.lojewski1998@gmail.com>
2022-01-28 00:09:47 +0100

    check all data about users

--- cypress/integration/api-tests/GetAllUsers.spec.js
+++ cypress/integration/api-tests/GetAllUsers.spec.js
@@ -1,19 +1,32 @@
+let totalPages = 0, countId = 0, iterator = 0;
 
-
-describe('GET', () => {
-    it('getAllUsers', () => {
-        cy.request('/api/users').should(res => {
-        const { page, per_page, total, total_pages, data } = res.body
-        const checkIds = () => data.forEach(({ id }, index) => {
-            expect(id).to.eq(++index);
+const checkResponseData = (response) => 
+response.body.data.forEach(({ id, email, first_name: firstName, last_name: lastName, avatar }) => {
+    expect(id).to.eq(++countId);
+    expect(email).to.not.eq('');
+    expect(firstName).to.not.eq('');
+    expect(lastName).to.not.eq('');
+    expect(avatar).to.not.eq('');
+})
+const checkAllPages = () => {
+    if(iterator !== totalPages) {
+        cy.request(`/api/users?page=${++iterator}`).should(res => {
+            expect(res.status).to.eq(200)
+                checkResponseData(res);
+                checkAllPages();
         })
-        checkIds();
+    }
+}
+
+before(() => {
+    cy.request('/api/users').should(res => {
         expect(res.status).to.eq(200)
-        expect(data.length).to.eq(data[data.length - 1].id)
-        expect(page).to.eq(1)
-        expect(per_page).to.eq(6)
-        expect(total).to.eq(12)
-        expect(total_pages).to.eq(2)
-        })
+    }).then(res => {
+        totalPages = res.body.total_pages;
     })
 })
+describe('GET', () => {
+    it('Check id for all users', () => {
+        checkAllPages();
+    })
+});
